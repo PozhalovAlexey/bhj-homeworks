df04aae4f568d4d80116d6160377c1d67402e69e
Alexey <113060366+PozhalovAlexey@users.noreply.github.com>
2023-07-22 15:02:39 +0300

    Little edit

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,18 +2,19 @@ const toolTips = document.querySelectorAll('.has-tooltip');
 
 toolTips.forEach(elem => {
     elem.addEventListener('click', (e) => {
-        e.preventDefault();
+        e.preventDefault()
         const tooltip = document.createElement('div');
         tooltip.textContent = elem.getAttribute('title');
         tooltip.classList.add('tooltip');
-        tooltip.classList.add('tooltip_active')
-        document.body.appendChild(tooltip)
-        console.log(tooltip.classList)
-        console.log(tooltip)
-        const removeToolTip = () => {
-            tooltip.remove()
-            document.removeEventListener('click', removeToolTip)
-        }
-        document.addEventListener('click', removeToolTip, {once: true})
-    })
-})
+        tooltip.classList.add('tooltip_active');
+        document.body.appendChild(tooltip);
+
+        const parentRect = elem.getBoundingClientRect();
+        tooltip.style.top = parentRect.bottom + 'px';
+        tooltip.style.left = parentRect.left + (parentRect.width - tooltip.offsetWidth)/2 + 'px';
+
+        elem.addEventListener('mouseover', () => {
+            tooltip.remove();
+        });
+    });
+});
