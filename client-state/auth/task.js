b3241abdd7f2f95bc77737706e00d8a2dab5c40d
Alexey <113060366+PozhalovAlexey@users.noreply.github.com>
2023-07-30 19:59:36 +0300

    Homework several adds

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -3,6 +3,18 @@ const form = document.getElementById('signin');
 const welcome = document.getElementById('welcome');
 const userId = document.getElementById('user_id');
 
+const checkUser = () => {
+    const storageUserId = localStorage.getItem('userId');
+    if (storageUserId !== null) {
+        userId.textContent = storageUserId;
+        welcome.classList.add('welcome_active');
+        form.classList.remove('signin_active')
+    } else {
+        form.classList.add('signin_active')
+    }
+}
+checkUser()
+
 signinForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(signinForm);
